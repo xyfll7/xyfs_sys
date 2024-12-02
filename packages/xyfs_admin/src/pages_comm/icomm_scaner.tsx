6427b70a461af2c39ea1734511372d172e52001d
小洋粉 <l.7@qq.com>
2024-12-02 09:13:47 +0800

    wip(project): 🚧 update

--- packages/xyfs_admin/src/pages_comm/icomm_scaner.tsx
+++ packages/xyfs_admin/src/pages_comm/icomm_scaner.tsx
@@ -200,7 +200,7 @@ const IIICameraScaner = (prams: { className?: string, onScanCode: (e: string) =>
   return <View className='dcl ww' style={{ height: '210rpx' }}>
     {getMyEnv().isUseInDev && <View className={`mb10 IOO ovh dxy  ${prams.className}`} style={{ height: '190rpx', width: '98vw', boxShadow: "0px 2rpx 8rpx 0px rgba(0, 0, 0, 0.3)", backgroundColor: "rgb(0 0 0 / 50%)" }}
       onClick={() => {
-        prams.onScanCode("551019962277");
+        prams.onScanCode("926678416066");
       }}>
       模拟扫码
     </View>
