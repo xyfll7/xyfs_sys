936001cd4fa2841ddb4008c907f863ae94fc548b
xyfll7 <l.7@qq.com>
2024-12-31 10:06:39 +0800

    wip(project): 🚧 update

--- packages/xyfs_admin/src/pages_comm/icomm_download_list.tsx
+++ packages/xyfs_admin/src/pages_comm/icomm_download_list.tsx
@@ -58,22 +58,29 @@ const Index: FC<{}> = ({ }) => {
                 throw new Error("正在下载，请稍后");
               }
             }}>
-              {e.type === 1 && <>
-                {differenceInMinutes(coo___ios_date(), coo___ios_date(e.createTime)) > ___time * 60 * 2 ? <Text className='cccplh'>该文件已过时，请去下载最新文件</Text> :
-                  <View className='dy'>
-                    {e.url && <><Text className='wm15rem nw1 '> {file_name}</Text><Text className='cccgreen'>  _完成</Text></>}
-                    {!e.url && <><Text className='wm15rem nw1'>{file_name}</Text><Text>_下载中...</Text></>}
-                  </View>
-                }
-              </>}
-              {e.type === 3 && <>
-                {differenceInMinutes(coo___ios_date(), coo___ios_date(e.createTime)) > ___time * 60 * 2 ? <Text className='cccplh'>该文件已过时，请去下载最新文件</Text> :
-                  <View className='dy'>
-                    {e.status == 1 && <><Text className='wm15rem nw1'>写入任务</Text><Text className='cccgreen'>_完成</Text></>}
-                    {e.status != 1 && <><Text className='wm15rem nw1'>写入任务</Text><Text>_执行中...</Text></>}
-                  </View>
-                }
-              </>}
+              {differenceInMinutes(coo___ios_date(), coo___ios_date(e.createTime)) > ___time * 60 * 2 ? <Text className='cccplh'>该文件已过时，请去下载最新文件</Text> :
+                <>
+                  {e.type === 1 &&
+                    <View className='dy'>
+                      {e.url && <><Text className='wm15rem nw1 '> {file_name}</Text><Text className='cccgreen'>  _完成</Text></>}
+                      {!e.url && <><Text className='wm15rem nw1'>{file_name}</Text><Text>_下载中...</Text></>}
+                    </View>
+                  }
+                  {e.type === 2 &&
+                    <View className='dy'>
+                      {e.url && <><Text className='wm15rem nw1 '> {file_name}</Text><Text className='cccgreen'>  _完成</Text></>}
+                      {!e.url && <><Text className='wm15rem nw1'>{file_name}</Text><Text>_下载中...</Text></>}
+                    </View>
+                  }
+                  {e.type === 3 &&
+                    <View className='dy'>
+                      {e.status == 1 && <><Text className='wm15rem nw1'>写入任务</Text><Text className='cccgreen'>_完成</Text></>}
+                      {e.status != 1 && <><Text className='wm15rem nw1'>写入任务</Text><Text>_执行中...</Text></>}
+                    </View>
+                  }
+                </>
+              }
+
             </ComButton>
             <ComButton rr ll>{i + 1}</ComButton>
           </View>
