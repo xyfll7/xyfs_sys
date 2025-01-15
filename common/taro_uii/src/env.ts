d178e6ef4657d6beab0e486207bcc830f2b1472f
xyfll7 <l.7@qq.com>
2025-01-15 14:23:11 +0800

    wip(project): 🚧 update

--- common/taro_uii/src/env.ts
+++ common/taro_uii/src/env.ts
@@ -3,7 +3,7 @@ import "@xyfs/utils/init";
 import Package from "../package.json";
 import { Environment, EnvVersion } from "../types/type_index";
 
-export const base_url: string = "http://192.168.60.229:8080";
+export const base_url: string = "http://192.168.60.229:8888";
 
 const ___envSimulate = "develop";
 
