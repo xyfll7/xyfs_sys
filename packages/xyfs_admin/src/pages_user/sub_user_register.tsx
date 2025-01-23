e69c0556b17efa8dd748c1f892863de65be35fbd
xyfll7 <l.7@qq.com>
2025-01-23 17:44:03 +0800

    wip(project): 🚧 update

--- packages/xyfs_admin/src/pages_user/sub_user_register.tsx
+++ packages/xyfs_admin/src/pages_user/sub_user_register.tsx
@@ -22,14 +22,14 @@ const Index: FC = () => {
     <ComNav>
       <ComNavBarA className='mb10 pl10'>
         <ComButton ll className='bcctrans cccplh ml10' >
-          {selfInfo_S.deptId ? '用户信息' : '报名团长'}
+          {selfInfo_S.deptId ? '我' : '报名团长'}
         </ComButton>
       </ComNavBarA>
     </ComNav>
     {selfInfo_S.deptId &&
       <ComScrollView>
         <>
-          <ComButton ll className='bccback mb10 prl10'>用户信息</ComButton>
+          <ComButton ll className='bccback mb10 prl10'>我的信息</ComButton>
           <View className='ioo bccwhite pt10 dll mb10 prl10 ww'>
             <View className='ww mb10 dy' >
               <ComButton ll className='w5rem bccwhite'>姓名</ComButton>
