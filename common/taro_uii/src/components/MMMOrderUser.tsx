68ad4acc6590b4f4d95fc7bf0c91156400bb4121
xyfll7 <l.7@qq.com>
2025-02-12 17:45:15 +0800

    wip(project): 🚧 update

--- common/taro_uii/src/components/MMMOrderUser.tsx
+++ common/taro_uii/src/components/MMMOrderUser.tsx
@@ -11,13 +11,12 @@ export const MMMOrderUser = ({ order, ...props }: ViewProps & { order: OrderInfo
   return <View className={`${props.className} dy`}>
     <ComImage className='mr10' src={order.userAvatar} />
     <ComButton ll className='dy cccplh'>
-      <Text className='wm5rem nw1' onClick={() => {
+      <Text className='wm8rem nw1' onClick={() => {
         Taro.setClipboardData({
           data: order.userName ? order.userName : "无",
           success: () => { Taro.showToast({ icon: "none", title: order.userName ? `已复制:${order.userName}/${order.deptName}` : "无" }); }
         });
-      }}>
-        {order.userName}/{order.deptName}
+      }}>{order.userName}/{order.deptName}
       </Text>
     </ComButton>
   </View>;
