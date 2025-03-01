425b34ff4f4572d22e1666b305b982804f90b9c8
小洋粉 <l.7@qq.com>
2025-03-01 11:14:24 +0800

    wip(project): 🚧 update

--- packages/taoding_knowledge_base_vite/src/App.tsx
+++ packages/taoding_knowledge_base_vite/src/App.tsx
@@ -27,41 +27,47 @@ function useLogin() {
   const [token, setToken] = useState<string>();
   useEffect(() => {
     let wwLogin: ww.WWLoginInstance;
-    function ww_login() {
-      return new Promise<string>((re, rj) => {
-        wwLogin = ww.createWWLoginPanel({
-          el: document.getElementById('login_box')!,
-          params: {
-            login_type: ww.WWLoginType.corpApp,
-            agentid: '1000052',
-            appid: 'ww9bfa0c5bd58bb8b3',
-            redirect_uri: 'http://file.taoding.cn',
-            state: 'STATE',
-            redirect_type: ww.WWLoginRedirectType.callback,
-          },
-          onCheckWeComLogin: (e) => {
-            console.log("islogin:;:", e.isWeComLogin);
-          },
-          onLoginSuccess: async ({ code, }) => {
-            const res_token = await login({ code });
-            console.log("code", code);
-            re(res_token.token);
-            wwLogin.unmount();
-          },
-          onLoginFail: async (err) => {
-            console.log(err);
-            rj(err);
-          }
+    const token_ = localStorage.getItem("token");
+    if (token_) {
+      setToken(token_);
+    } else {
+      function ww_login() {
+        return new Promise<string>((re, rj) => {
+          wwLogin = ww.createWWLoginPanel({
+            el: document.getElementById('login_box')!,
+            params: {
+              login_type: ww.WWLoginType.corpApp,
+              agentid: '1000052',
+              appid: 'ww9bfa0c5bd58bb8b3',
+              redirect_uri: 'http://file.taoding.cn',
+              state: 'STATE',
+              redirect_type: ww.WWLoginRedirectType.callback,
+            },
+            onCheckWeComLogin: (e) => {
+              console.log("islogin:;:", e.isWeComLogin);
+            },
+            onLoginSuccess: async ({ code, }) => {
+              const res_token = await login({ code });
+              console.log("code", code);
+              re(res_token.token);
+              wwLogin.unmount();
+            },
+            onLoginFail: async (err) => {
+              console.log(err);
+              rj(err);
+            }
+          });
         });
-      });
+      }
+      (async () => {
+        const res_token = await ww_login();
+        localStorage.setItem("token", res_token);
+        setToken(res_token);
+      })();
     }
-    (async () => {
-      const res_token = await ww_login();
-      localStorage.setItem("token", res_token);
-      setToken(res_token);
-    })();
+
     return () => {
-      wwLogin.unmount();
+      wwLogin?.unmount();
     };
   }, []);
   return token;
