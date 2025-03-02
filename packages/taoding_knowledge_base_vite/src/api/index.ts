fac9f0e4798fda00a39a84938db2e7ec21037f9b
小洋粉 <l.7@qq.com>
2025-03-02 21:58:34 +0800

    wip(project): 🚧 update

--- packages/taoding_knowledge_base_vite/src/api/index.ts
+++ packages/taoding_knowledge_base_vite/src/api/index.ts
@@ -1,9 +1,17 @@
 import qs from "qs";
 import { Cate, MyFile, User } from "../vite-env";
-// const base_url = 'http://192.168.60.230:8081';
 
-// const base_url = 'http://file.taoding.cn:8081';
-const base_url = 'https://file-share.taoding.cn';
+
+
+const base_url = (() => {
+  console.log(",,,,,", process.env.NODE_ENV);
+  if (process.env.NODE_ENV === "development") {
+    return 'http://192.168.60.230:8081';
+  } else {
+    return 'https://file-share.taoding.cn';
+    // return 'http://file.taoding.cn:8081';
+  }
+})();
 
 async function base_fetch_new<T>(url: string, params: Record<string, any>) {
   console.log(params);
