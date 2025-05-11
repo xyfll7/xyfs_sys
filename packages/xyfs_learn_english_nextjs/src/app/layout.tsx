517713e47fdcf1b22aea9ae750d2c60c5c6d84c4
xyf <l.7@qq.com>
2025-05-11 22:06:59 +0800

    wip(project): 🚧 update

--- packages/xyfs_learn_english_nextjs/src/app/layout.tsx
+++ packages/xyfs_learn_english_nextjs/src/app/layout.tsx
@@ -1,17 +1,17 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="zh-CN" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} flex flex-col w-full  justify-between antialiased overflow-hidden h-screen max-h-screen`}
+        className={`antialiased overflow-hidden h-screen max-h-screen`}
       >
         <ThemeProvider
           attribute="class"
--- packages/xyfs_learn_english_nextjs/src/app/page.tsx
+++ packages/xyfs_learn_english_nextjs/src/app/page.tsx
@@ -1,37 +1,73 @@
+"use client";
 import { Fragment } from "react";
-import { getDictionary } from "./dictionaries";
 
 
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { WheelGesturesPlugin } from 'embla-carousel-wheel-gestures';
 
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
 
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Separator } from "@/components/ui/separator";
+
 
 const tags = Array.from({ length: 50 }).map(
   (_, i, a) => `v1.2.0-beta.${a.length - i}`
 );
 
-export default async function Home() {
-
-
-  const dict = await getDictionary(); // en
+export default function Home() {
   return (
     <>
-      <div className="flex flex-1  w-full  overflow-hidden">
-        <aside className="w-[33.333vw]">
-          <div>
-            <button>{dict.products.cart}</button>
+      <ResizablePanelGroup direction="horizontal">
+        <ResizablePanel className="flex flex-col h-screen max-h-screen overflow-hidden">
+          <div className="flex-1 flex flex-col items-center justify-center overflow-hidden">
+
+            <Carousel className="bg-green-300 w-full h-full flex flex-col justify-center overflow-hidden" orientation="vertical"
+              plugins={[WheelGesturesPlugin()]} >
+              <CarouselContent className="bg-red-300 h-full flex-1">
+                <CarouselItem className="bg-blue-300 h-full">
+                  111  <video className="bccred w-full" src="/video0.mp4" controls></video>
+                </CarouselItem>
+                <CarouselItem className="bg-red-500 h-full">
+                  222  {/* <video className="bccred w-full" src="/video0.mp4" controls></video> */}
+                </CarouselItem>
+                <CarouselItem className="bg-yellow-300 h-full">
+                  333  {/* <video className="bccred w-full" src="/video0.mp4" controls></video> */}
+                </CarouselItem>
+              </CarouselContent>
+            </Carousel>
           </div>
-        </aside>
-        <div className="  h-full flex w-full flex-col overflow-hidden">
-          <header>网站头部11</header>
-          <ScrollAreaDemo></ScrollAreaDemo>
-          <header></header>
-        </div>
-      </div>
-      <footer className="">
-      </footer>
+          <Header></Header>
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel className="flex flex-col h-screen max-h-screen overflow-hidden">
+          <ScrollAreaDemo />
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </>
+  );
+}
+
+function Header() {
+  return (
+    <div className="flex items-center justify-between p-4  border-b">
+      <div className="flex items-center space-x-2">
+        <h1 className="text-lg font-bold">Header</h1>
+      </div>
+      <div className="flex items-center space-x-2">
+        <button className="px-4 py-2 text-sm font-medium text-white bg-blue-500 rounded-md">
+          Button
+        </button>
+      </div>
+    </div>
   );
 }
 
@@ -39,7 +75,7 @@ function ScrollAreaDemo() {
   return (
     <ScrollArea className="w-full flex-1  overflow-hidden">
       <div className="flex w-full">
-        <div className="p-4 w-full bg-red-500 ">
+        <div className="p-4 w-full  ">
           <h4 className="mb-4 text-sm font-medium leading-none">Tags</h4>
           {tags.map((tag) => (
             <Fragment key={tag}>
@@ -50,7 +86,7 @@ function ScrollAreaDemo() {
             </Fragment>
           ))}
         </div>
-        <div className="w-[33.333vw] bg-yellow-700">fasdf</div>
+        <div className="w-[33.333vw] ">fasdf</div>
       </div>
     </ScrollArea>
   );
