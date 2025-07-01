c2984feb00a5dbff7a7c773294fa3d3868b7342e
xyfll7 <l.7@qq.com>
2025-07-01 20:19:22 +0800

    wip(project): 🚧 update

--- packages/xyfs_admin/src/pages_comm/icomm_product_list.tsx
+++ packages/xyfs_admin/src/pages_comm/icomm_product_list.tsx
@@ -149,9 +149,10 @@ const IIIStock = ({ onClose, product, onUpdateStock }: { onClose: () => void; pr
     </View>
     <View>
       <ComButton className='cccplh bccback' hoverClass="none">
-        当前库存 {product.stock} {Number(stock) - Number(product.stock) > 0 ? "+" : "-"} {Math.abs(Number(stock) - Number(product.stock))}
+        <Text>当前库存 {product.stock}</Text>
+        <Text className="w1rem dxy">{Number(stock) - Number(product.stock) < 0 ? "-" : "+"}</Text>
+        <Text>{Math.abs(Number(stock) - Number(product.stock))}</Text>
       </ComButton>
-      <ComButton className='cccplh bccback mb10' hoverClass="none">改后库存 {Number(stock)} </ComButton>
     </View>
     <View className='ww prl10 dr'>
       <ComButton className='mb10   bccgreen  dxy cccwhite' onClick={async () => {
