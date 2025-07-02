ec8f2139e17deb4404c9e5b6c30969604adf43e0
xyfll7 <l.7@qq.com>
2025-07-02 20:26:04 +0800

    wip(project): 🚧 update

--- packages/xyfs_admin/src/pages_comm/icomm_product_list.tsx
+++ packages/xyfs_admin/src/pages_comm/icomm_product_list.tsx
@@ -46,7 +46,7 @@ const Index: FC = () => {
   return <MMMAAPage isNeedRegiment={false} >
     <ComNav className="prl10 ww">
       <ComNavBarA className='mb10'>
-        <ComButton ll className='bcctrans ml10 cccplh' hoverClass="none">商品列表1</ComButton>
+        <ComButton ll className='bcctrans ml10 cccplh' hoverClass="none">商品列表</ComButton>
       </ComNavBarA>
       <View className="dbtc ww">
         <ComListTypeSelectorNew disabled={page_loading} enumData={{ "1": "销售中", "-1": "已下架", 销售中: 1, 已下架: -1 }} typeList={[1, -1]}
