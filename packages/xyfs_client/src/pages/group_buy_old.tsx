644521b38704adcc23db67995222fb456b1cee2f
xyfll7 <l.7@qq.com>
2025-07-18 10:29:19 +0800

    fix(group_buy): update page identifier for group_buy_old

--- packages/xyfs_client/src/pages/group_buy_old.tsx
+++ packages/xyfs_client/src/pages/group_buy_old.tsx
@@ -1,4 +1,4 @@
-// :: pages/group_buy
+// :: pages/group_buy_old
 import { Text, View } from '@tarojs/components';
 import Taro, { useDidShow } from '@tarojs/taro';
 import { Api_goods_groupBuyingUserList_ctn, Api_goods_list_ctn, Api_goodsCart_preOrder_ctn } from "@xyfs/taro_uii/api/api__goods";
