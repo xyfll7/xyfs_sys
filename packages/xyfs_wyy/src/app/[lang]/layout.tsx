b8e7e885e954a5a18a9ee5f87b54bb5fd638f0a5
xyfll7 <l.7@qq.com>
2025-08-22 09:04:02 +0800

    wip(project): 🚧 update

--- packages/xyfs_wyy/src/app/[lang]/layout.tsx
+++ packages/xyfs_wyy/src/app/[lang]/layout.tsx
@@ -1,5 +1,4 @@
 import { ThemeProvider } from "@/components/theme-provider";
-import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Lang } from "../../middleware";
 import "./app.css";
@@ -14,23 +13,21 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-// 元数据如何根据 lang 动态切换
-const metadataMap: Record<Lang, Metadata> = {
-  en: {
-    title: "How to Say in English",
-    description: "let people say English in life scene",
-    keywords: ["English", "say English", "How to Say in English"],
-  },
-  zh: {
-    title: "英语怎么说",
-    description: "让人们在生活场景中开口说英语",
-    keywords: ["英语", "说英语", "英语怎么说"],
-  },
-};
 
 // 动态生成元数据
-export async function generateMetadata({ params }: { params: { lang: Lang; }; }) {
-  return metadataMap[params.lang];
+export async function generateMetadata({ params }: { params: Promise<{ lang: Lang; }>; }) {
+  return {
+    en: {
+      title: "How to Say in English",
+      description: "let people say English in life scene",
+      keywords: ["English", "say English", "How to Say in English"],
+    },
+    zh: {
+      title: "英语怎么说",
+      description: "让人们在生活场景中开口说英语",
+      keywords: ["英语", "说英语", "英语怎么说"],
+    },
+  }[(await params).lang];
 }
 
 
