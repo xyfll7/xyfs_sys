7dc837acae42986880a0c92d80e5aafa109b6e06
xyfll7 <l.7@qq.com>
2025-08-28 18:49:39 +0800

    wip(project): 🚧 update

--- packages/xyfs_cli/src/utils/mcp_test.ts
+++ packages/xyfs_cli/src/utils/mcp_test.ts
@@ -1,19 +1,46 @@
 import { ChatOpenAI } from "@langchain/openai";
+import 'dotenv/config';
 import { z } from "zod";
 
-
+console.log("Testing MCP Client...", process.env.DASHSCOPE_API_KEY);
 const model = new ChatOpenAI({
-  model: "gpt-4o-mini",
-  apiKey: process.env.OPENAI_API_KEY!,
-  temperature: 0
+  model: "qwen-plus-latest",
+  apiKey: process.env.DASHSCOPE_API_KEY!,
+  temperature: 0,
+  configuration: {
+    baseURL: "https://dashscope.aliyuncs.com/compatible-mode/v1",
+  },
+  modelKwargs: {
+    max_tokens: 1500,
+    type: "json_object"
+  }
 });
 
-const joke = z.object({
-  setup: z.string().describe("The setup of the joke"),
-  punchline: z.string().describe("The punchline to the joke"),
-  rating: z.number().optional().describe("How funny the joke is, from 1 to 10"),
-});
 
-const structuredLlm = model.withStructuredOutput(joke);
+await test_1();
 
-await structuredLlm.invoke("Tell me a joke about cats");
+
+async function test_1() {
+  const res = await model.invoke(`attractions`);
+  console.log("Joke:", res.content);
+}
+
+
+async function test_0() {
+  const joke = z.object({
+    setup: z.string().describe("The setup of the joke"),
+    punchline: z.string().describe("The punchline to the joke"),
+    rating: z.number().describe("How funny the joke is, from 1 to 10"),
+  });
+
+  const structuredLlm = model.withStructuredOutput(joke, {
+    method: "json_object",
+    name: "joke"
+  });
+
+
+  const res = await structuredLlm.invoke(`Tell me a joke about cats.`);
+
+  console.log("Joke:", res.setup, res.punchline, "Rating:", res.rating);
+}
+
