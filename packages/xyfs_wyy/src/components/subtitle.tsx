54a5c0c84fdd7e95f661685998dfa1b32c29ad20
xyfll7 <l.7@qq.com>
2025-09-03 17:48:59 +0800

    wip(project): 🚧 update

--- packages/xyfs_wyy/src/components/subtitle.tsx
+++ packages/xyfs_wyy/src/components/subtitle.tsx
@@ -1,13 +1,39 @@
 "use client";
 import { MOCK_SAMPLE_ASS } from "../lib/mock";
 
+import { Message, Ollama } from 'ollama/browser';
+
+
+const ollama = new Ollama({
+  host: 'http://127.0.0.1:11434',
+  headers: {
+    Authorization: 'Bearer <api key>',
+    'User-Agent': 'MyApp/1.0',
+  },
+});
+
+
 export function Subtitle() {
   const handleMouseUp = () => {
     const selectedText = window.getSelection()?.toString();
     if (selectedText && selectedText.trim() !== '') {
       console.log('选中的文字:', selectedText);
+      const message = { role: 'user', content: `${selectedText} 讲解该单词` };
+      abc(message);
     }
   };
+
+  async function abc(message: Message) {
+    const response = await ollama.chat({
+      model: 'llama3.2',
+      messages: [message],
+      stream: true,
+    });
+    for await (const part of response) {
+      // 此处是一个流，我想讲它渲染到页面中去
+      console.log(part.message.content);
+    }
+  }
 
   return (
     <div className="flex flex-col" onMouseUp={handleMouseUp}>
