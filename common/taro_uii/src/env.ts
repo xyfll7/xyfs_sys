74ff228b38fc107eb2b68c9d887232fa29777465
xyfll7 <l.7@qq.com>
2025-11-20 11:28:30 +0800

    wip(project): 🚧 update

--- common/taro_uii/src/env.ts
+++ common/taro_uii/src/env.ts
@@ -10,7 +10,7 @@ const ___envSimulate = "release";
 // cSpell: disable;
 const OPENID_DEV___ =
   // @ts-ignore
-  // "18609115888" ||  //
+  // "15291119896" ||  //
   ////////////////////////////////////////////////
   // "oGwbL5D1WtsGTuLhWneaepYuHW3k" ||  // 何旭东
   // "oGwbL5OLhRpOKRWrkVNSMqXuGPaE" ||  // 我妈
